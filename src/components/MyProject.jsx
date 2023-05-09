5ef7230e02b0301920284452a53813dd53d52faa
minjaechoi0518 <cmk7892@gmail.com>
2023-05-10 05:46:22 +0900

    ㅜ

--- src/components/MyProject.jsx
+++ src/components/MyProject.jsx
@@ -1,65 +1,65 @@
-import styled from "styled-components";
-import { Button } from "./component/style";
-import { useNavigate } from "react-router-dom";
-import { useQuery } from "react-query";
-import { allSprint, myProject } from "../axios/api";
-import { useState } from "react";
-import Cookies from "js-cookie";
-import * as CSS from "../components/component/style";
+// import styled from "styled-components";
+// import { Button } from "./component/style";
+// import { useNavigate } from "react-router-dom";
+// import { useQuery } from "react-query";
+// import { allSprint, myProject } from "../axios/api";
+// import { useState } from "react";
+// import Cookies from "js-cookie";
+// import * as CSS from "../components/component/style";
 
-const AllSprintList = () => {
-  const navigate = useNavigate();
-  // const [isVisible, setIsVisible] = useState('')
-  //API 연결
-  const { isLoading, isError, data } = useQuery("myProject", myProject);
-  // debugger;
+// const AllSprintList = () => {
+//   const navigate = useNavigate();
+//   // const [isVisible, setIsVisible] = useState('')
+//   //API 연결
+//   const { isLoading, isError, data } = useQuery("myProject", myProject);
+//   // debugger;
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error: {Error.message}</div>;
-  const detailOpen = (sprintId) => {
-    // if (!!Cookies.get('token')) {
-    //   alert('로그인이 필요합니다.')
-    //   return;
-    // }
-    navigate(`/main/${sprintId}`);
-  };
+//   if (isLoading) return <div>Loading...</div>;
+//   if (isError) return <div>Error: {Error.message}</div>;
+//   const detailOpen = (sprintId) => {
+//     // if (!!Cookies.get('token')) {
+//     //   alert('로그인이 필요합니다.')
+//     //   return;
+//     // }
+//     navigate(`/main/${sprintId}`);
+//   };
 
-  return (
-    <CSS.CardLists>
-      {data.map((item) => (
-        <CSS.Cards key={item.sprintId}>
-          <CSS.CardTitles>{item.title}</CSS.CardTitles>
-          <CSS.CardContent>
-            <div>{` ${item.nickname}`}</div>
-            <div>{` ${item.sprintType}`}</div>
-            <div>{` ${item.numLikes}`}</div>
-            <CSS.CardField>
-              <CSS.CardFieldTitle>모집인원:</CSS.CardFieldTitle>
-              {item.fieldObjectList.map((field) => (
-                <CSS.CardFieldContent key={field.fieldName}>
-                  <div>{`${field.fieldName}: `}</div>
-                  <div>{`${field.nowMemberCount}/${field.fieldMaxNum}`}</div>
+//   return (
+//     <CSS.CardLists>
+//       {data.map((item) => (
+//         <CSS.Cards key={item.sprintId}>
+//           <CSS.CardTitles>{item.title}</CSS.CardTitles>
+//           <CSS.CardContent>
+//             <div>{` ${item.nickname}`}</div>
+//             <div>{` ${item.sprintType}`}</div>
+//             <div>{` ${item.numLikes}`}</div>
+//             <CSS.CardField>
+//               <CSS.CardFieldTitle>모집인원:</CSS.CardFieldTitle>
+//               {item.fieldObjectList.map((field) => (
+//                 <CSS.CardFieldContent key={field.fieldName}>
+//                   <div>{`${field.fieldName}: `}</div>
+//                   <div>{`${field.nowMemberCount}/${field.fieldMaxNum}`}</div>
 
-                  <CSS.CardFieldBar>
-                    <CSS.CardFieldBarFill
-                      fill={field.nowMemberCount}
-                      max={field.fieldMaxNum}
-                    />
-                  </CSS.CardFieldBar>
-                </CSS.CardFieldContent>
-              ))}
-            </CSS.CardField>
-          </CSS.CardContent>
+//                   <CSS.CardFieldBar>
+//                     <CSS.CardFieldBarFill
+//                       fill={field.nowMemberCount}
+//                       max={field.fieldMaxNum}
+//                     />
+//                   </CSS.CardFieldBar>
+//                 </CSS.CardFieldContent>
+//               ))}
+//             </CSS.CardField>
+//           </CSS.CardContent>
 
-          {/* <Link to={`/main/${item.sprintId}`}> */}
-          <Button onClick={() => detailOpen(item.sprintId)} type="positive">
-            more
-          </Button>
-          {/* </Link> */}
-        </CSS.Cards>
-      ))}
-    </CSS.CardLists>
-  );
-};
+//           {/* <Link to={`/main/${item.sprintId}`}> */}
+//           <Button onClick={() => detailOpen(item.sprintId)} type="positive">
+//             more
+//           </Button>
+//           {/* </Link> */}
+//         </CSS.Cards>
+//       ))}
+//     </CSS.CardLists>
+//   );
+// };
 
-export default AllSprintList;
+// export default AllSprintList;
